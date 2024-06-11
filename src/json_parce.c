6c09991b5d4ff6ef2f230bf92c50c475fa4f8fc6
Nick Fistere <nwfistere@gmail.com>
2024-06-11 08:48:06 -0500

    When decoding json strings, we need to remove the \ prior to '"' characters.

--- src/json_parce.c
+++ src/json_parce.c
@@ -1310,10 +1310,39 @@ JSON_PARCE_API size_t json_deep_parce_execute_file(
   return do_json_parce_execute_file(parser, callbacks, file, 1);
 }
 
+static size_t decode_string(char* str, size_t len) {
+  char* p = str;
+
+  for (size_t i = 0; i < len; i++) {
+    if (str[i] == '\\') {
+      char next = str[i + 1];
+      switch(str[i + 1]) {
+        case '"': {
+          *p = next;
+          i++;
+          break;
+        }
+      }
+    } else {
+      *p = str[i];
+    }
+    p++;
+  }
+  *p = '\0';
+
+  return strlen(str);
+}
+
 JSON_PARCE_API char *json_parce_string(const char *str, size_t len) {
   char *ret = (char *)malloc((len + 1) * sizeof(char));
   memcpy(ret, str, len);
   ret[len] = '\0';
+
+  if (memchr(ret, '\\', len) != NULL) {
+    // need to decode characters.
+    len = decode_string(ret, len);
+  }
+
   return ret;
 }
 
